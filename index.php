8257324c92fcc506dee0c2349bdf906dcd794ba1
lucacappello79 <postt79@gmail.com>
2023-04-18 14:46:50 +0200

    init exercise

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTELS</title>
+</head>
+<body>
+
+<h1>Hotels</h1>
+
+<?php 
+
+foreach ($hotels as $item) {
+    echo '<strong>Hotel Name</strong>' . ": " . $item['name'] . "<br>";
+    echo '<strong>Features</strong>' . ": " . $item['description'] . "<br>";
+    echo '<strong>Parking</strong>' . ": " . ($item['parking'] ? 'Y' : 'N') . "<br>";
+    echo '<strong>Rating</strong>' . ": " . $item['vote'] . "<br>";
+    echo '<strong>Distance</strong>' . ": " . $item['distance_to_center'] . "<br><br>";
+    
+}
+?>
+
+    
+</body>
+</html>
